b224b8c932edd7ae3003a8eb7eada6c3b580540b
Looseminded <31506967+Looseminded@users.noreply.github.com>
2023-02-02 00:28:44 -0800

    Add files via upload
    Cleaned up some code and made it easier to read.

--- minorRolls.py
+++ minorRolls.py
@@ -11,65 +11,65 @@ def chooseDifficulty():
 	diffList = ['Adept', 'Expert', 'Master', 'Legendary']
 	pickedDiff = system_random.choice(diffList)
 	return pickedDiff
-	print("Difficulty: " + pickedDiff)
+	print(f"Difficulty: {pickedDiff}")
 
 def chooseDragonSouls():
 	pickedDragonSouls = random.randint(0, 5)
 	return pickedDragonSouls
-	print("Starting Dragon Souls: " + str(pickedDragonSouls))
+	print(f"Starting Dragon Souls: {pickedDragonSouls}")
 	
 def chooseHealth():
 	pickedHealth = random.randint(-20, 20)
 	return pickedHealth
-	print("Modified Health: " + pickedHealth)
+	print(f"Modified Health: {pickedHealth}")
 	
 def chooseHealRate():
 	pickedHealRate = random.randint(-20, 20)
 	formattedHealRate = "{:.2f}".format(pickedHealRate / 100)
 	return formattedHealRate
-	print("Modified Heal Rate: " + formattedHealRate)
+	print(f"Modified Heal Rate: {formattedHealRate}")
 	
 def chooseHealRateMult():
 	pickedHRateMult = random.randint(-35, 35)
 	return pickedHRateMult
-	print("Modified Heal Mult: " + pickedHRateMult)
+	print(f"Modified Heal Mult: {pickedHRateMult}")
 
 def chooseStamina():
 	pickedStamina = random.randint(-20, 20)
 	return pickedStamina
-	print("Modified Stamina: " + pickedStamina)
+	print(f"Modified Stamina: {pickedStamina}")
 	
 def chooseStamRate():
 	pickedStamRate = random.randint(-20, 20)
 	formattedStamRate = "{:.2f}".format(pickedStamRate / 100)
 	return formattedStamRate
-	print("Modified Stamina Rate: " + formattedStamRate)
+	print(f"Modified Stamina Rate: {formattedStamRate}")
 	
 def chooseStamMult():
 	pickedStamMult = random.randint(-35, 35)
 	return pickedStamMult
-	print("Modified Stamina Rate: " + pickedStamMult)
+	print(f"Modified Stamina Rate: {pickedStamMult}")
 
 def chooseMagicka():
 	pickedMagicka = random.randint(-20, 20)
 	return pickedMagicka
-	print("Modified Magicka: " + pickedMagicka)
+	print(f"Modified Magicka: {pickedMagicka}")
 	
 def chooseMagickaRate():
 	pickedMagickaRate = random.randint(-20, 20)
 	formattedMagickaRate = "{:.2f}".format(pickedMagickaRate / 100)
 	return formattedMagickaRate
-	print("Modified Magicka Rate: " + formattedMagickaRate)
+	print(f"Modified Magicka Rate: {formattedMagickaRate}")
 	
 def chooseMagickaMult():
 	pickedMagickaMult = random.randint(-35, 35)
 	return pickedMagickaMult
-	print("Modified Magicka Rate: " + pickedMagickaMult)
+	print(f"Modified Magicka Rate: {pickedMagickaMult}")
 	
 def chooseSpeed():
 	pickedSpeed = random.randint(-20, 20)
 	return pickedSpeed
-	print("Modified Speed Mult: " + pickedSpeed)
+	print(f"Modified Speed Mult: {pickedSpeed}")
 	
 def chooseWSpeed():
 	pickedWSpeed = random.randint(-20, 20)
@@ -78,91 +78,91 @@ def chooseWSpeed():
 	baseValue += 1.0
 	formattedWSpeed = "{:.2f}".format(baseValue)
 	return formattedWSpeed
-	print("Modified Weapon Speed: " + formattedWSpeed)
+	print(f"Modified Weapon Speed: {formattedWSpeed}")
 	
 def chooseADamage():
 	pickedADamage = random.randint(-35, 35)
 	formattedADamage = "{:.2f}".format(pickedADamage / 100)
 	return formattedADamage
-	print("Modified Attack Damage: " + formattedADamage)
+	print(f"Modified Attack Damage: {formattedADamage}")
 	
 def chooseCrit():
 	pickedCrit = random.randint(0, 10)
 	return pickedCrit
-	print("Modified Crit Chance: " + pickedCrit)
+	print(f"Modified Crit Chance: {pickedCrit}")
 	
 def chooseUnarmed():
 	pickedUnarmed = random.randint(0, 10)
 	return pickedUnarmed
-	print("Modified Unarmed Damage: " + pickedUnarmed)
+	print(f"Modified Unarmed Damage: {pickedUnarmed}")
 	
 def chooseShoutMult():
 	pickedShoutMult = random.randint(-30, 30)
 	formattedShoutMult = "{:.2f}".format(pickedShoutMult / 100)
 	return formattedShoutMult
-	print("Modified Shout Mult: " + formattedShoutMult)
+	print(f"Modified Shout Mult: {formattedShoutMult}")
 	
 def chooseAlteration():
 	pickedAlteration = random.randint(-15, 15)
 	return pickedAlteration
-	print("Modified Alteration Power: " + pickedAlteration)
+	print(f"Modified Alteration Power: {pickedAlteration}")
 	
 def chooseConjuration():
 	pickedConjuration = random.randint(-15, 15)
 	return pickedConjuration
-	print("Modified Conjuration Power: " + pickedConjuration)
+	print(f"Modified Conjuration Power: {pickedConjuration}")
 	
 def chooseDestruction():
 	pickedDestruction = random.randint(-15, 15)
 	return pickedDestruction
-	print("Modified Destruction Power: " + pickedDestruction)
+	print(f"Modified Destruction Power: {pickedDestruction}")
 	
 def chooseIllusion():
 	pickedIllusion = random.randint(-15, 15)
 	return pickedIllusion
-	print("Modified Illusion Power: " + pickedIllusion)
+	print(f"Modified Illusion Power: {pickedIllusion}")
 	
 def chooseRestoration():
 	pickedRestoration = random.randint(-15, 15)
 	return pickedRestoration
-	print("Modified Restoration Power: " + pickedRestoration)
+	print(f"Modified Restoration Power: {pickedRestoration}")
 	
 def chooseEnchanting():
 	pickedEnchanting = random.randint(-25, 25)
 	return pickedEnchanting
-	print("Modified Enchanting Power: " + pickedEnchanting)
+	print(f"Modified Enchanting Power: {pickedEnchanting}")
 	
 def chooseDisease():
 	pickedDisease = random.randint(-10, 10)
 	return pickedDisease
-	print("Modified Disease Resist: " + pickedDisease)
+	print(f"Modified Disease Resist: {pickedDisease}")
 	
 def choosePoison():
 	pickedPoison = random.randint(-10, 10)
 	return pickedPoison
-	print("Modified Poison Resist: " + pickedPoison)
+	print(f"Modified Poison Resist: {pickedPoison}")
 	
 def chooseFire():
 	pickedFire = random.randint(-10, 10)
 	return pickedFire
-	print("Modified Fire Resist: " + pickedFire)
+	print(f"Modified Fire Resist: {pickedFire}")
 	
 def chooseShock():
 	pickedShock = random.randint(-10, 10)
 	return pickedShock
-	print("Modified Shock Resist: " + pickedShock)
+	print(f"Modified Shock Resist: {pickedShock}")
 	
 def chooseFrost():
 	pickedFrost = random.randint(-10, 10)
 	return pickedFrost
-	print("Modified Frost Resist: " + pickedFrost)
+	print(f"Modified Frost Resist: {pickedFrost}")
 	
 def chooseMagic():
 	pickedMagic = random.randint(-10, 10)
 	return pickedMagic
-	print("Modified Magic Resist: " + pickedMagic)
+	print(f"Modified Magic Resist: {pickedMagic}")
 	
 def chooseAbsorb():
 	pickedAbsorb = random.randint(0, 5)
 	return pickedAbsorb
-	print("Modified Absorb Chance: " + pickedAbsorb)
+	print(f"Modified Absorb Chance: {pickedAbsorb}")
